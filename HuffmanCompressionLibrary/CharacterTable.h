e5716a403bf9d460e8a91a150bfa1a3b4d27c961
Shailendra Singh <computerface56@hotmail.com>
2022-07-23 02:01:39 -0400

    Added square bracket operator without returning a reference, for CharacterTable.

--- HuffmanCompressionLibrary/CharacterTable.h
+++ HuffmanCompressionLibrary/CharacterTable.h
@@ -14,6 +14,7 @@ public:
 	CharacterTable(std::string DecompressedFilePath);
 
 	//Operator overloads---------------------------------------------------------------------------------------------------------
+	CharacterTableNode operator[](unsigned char key) const;
 	CharacterTableNode& operator[](unsigned char key);
 
 };
